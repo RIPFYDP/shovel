5d2060d4ab98735d657585d33a6fc0cec680073b
Jason Kim <jasonkim@rhapsody.com>
2015-02-22 18:03:01 -0800

    Dashboard

--- app/controllers/control_panel_controller.js
+++ app/controllers/control_panel_controller.js
@@ -1,6 +1,26 @@
+var Q = require('q');
+var Webpage = require('../models/webpage');
+var Entity = require('../models/entity');
+
 var controlPanelController = {
   index: function(req, res, next) {
-    res.render('control_panel/index');
+    var data = {};
+
+    Webpage.findAllQ()
+    .then(function(webpages) {
+      data.webpages = webpages;
+      return Entity.findAllQ();
+    }, function(err) {
+      req.flash('danger', 'Webpages didn\'t load. Check if mongodb is up.');
+      return res.redirect('/');
+    })
+    .then(function(entities) {
+      data.entities = entities;
+      res.render('control_panel/index', data);
+    }, function(err) {
+      req.flash('danger', 'Entities didn\'t load.');
+      return res.redirect('/');
+    });
   }
 };
 
